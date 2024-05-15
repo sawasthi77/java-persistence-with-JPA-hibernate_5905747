3a3d2650039123f03fac7a38a7497b829eb40733
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-15 06:28:00 +0000

    Add code to create EM

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,7 +1,13 @@
 package com.mycompany.app;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello from Java!");
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_persistence_unit");
+
+    EntityManager em = emf.createEntityManager(); // Represent the persistence context
   }
 }
