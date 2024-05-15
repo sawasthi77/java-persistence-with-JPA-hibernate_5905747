5e796b8da7c5e041f95169577147f6317fd4a0c4
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-15 05:04:00 +0000

    End state for 03_03

---
+++ src/main/java/com/mycompany/app/persistence/DemoPersistenceUnitInfo.java
@@ -0,0 +1,123 @@
+package com.mycompany.app.persistence;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import jakarta.persistence.SharedCacheMode;
+import jakarta.persistence.ValidationMode;
+import jakarta.persistence.spi.ClassTransformer;
+import jakarta.persistence.spi.PersistenceUnitInfo;
+import jakarta.persistence.spi.PersistenceUnitTransactionType;
+
+// Remember to comment this class if you are using the persistence.xml file
+public class DemoPersistenceUnitInfo implements PersistenceUnitInfo {
+
+  @Override
+  public void addTransformer(ClassTransformer transformer) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean excludeUnlistedClasses() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public ClassLoader getClassLoader() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<URL> getJarFileUrls() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public DataSource getJtaDataSource() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  // Remember to implement this method. If not things will not work
+  @Override
+  public List<String> getManagedClassNames() {
+    return null;
+  }
+
+  @Override
+  public List<String> getMappingFileNames() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ClassLoader getNewTempClassLoader() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public DataSource getNonJtaDataSource() {
+    HikariDataSource dataSource = new HikariDataSource();
+
+    dataSource.setJdbcUrl("jdbc:mariadb://localhost:3306/library");
+    dataSource.setUsername("root");
+    dataSource.setPassword("test");
+    return dataSource;
+  }
+
+  @Override
+  public String getPersistenceProviderClassName() {
+    return "org.hibernate.jpa.HibernatePersistenceProvider";
+  }
+
+  @Override
+  public String getPersistenceUnitName() {
+    return "library_persistence_unit";
+  }
+
+  @Override
+  public URL getPersistenceUnitRootUrl() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public String getPersistenceXMLSchemaVersion() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Properties getProperties() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public SharedCacheMode getSharedCacheMode() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public PersistenceUnitTransactionType getTransactionType() {
+    return PersistenceUnitTransactionType.RESOURCE_LOCAL;
+  }
+
+  @Override
+  public ValidationMode getValidationMode() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
