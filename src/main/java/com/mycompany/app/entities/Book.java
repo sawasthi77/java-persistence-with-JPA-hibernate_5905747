3036c052a4e870d28a1d1bc7af31d0bd61db410a
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-15 12:55:58 +0000

    04_02 end state

--- src/main/java/com/mycompany/app/entities/Book.java
+++ src/main/java/com/mycompany/app/entities/Book.java
@@ -1,8 +1,41 @@
 package com.mycompany.app.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Book {
+
+  @Column(name = "book_id")
+  private int id;
+
+  @Column(name = "book_name")
+  private String name;
+
+  private String isbn;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
 
 }
