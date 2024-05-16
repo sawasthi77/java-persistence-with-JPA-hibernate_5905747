fa2d1f62a4ecbd00713bb20a9bc096789b08bd76
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-16 05:43:21 +0000

    04_03 end state

--- src/main/java/com/mycompany/app/entities/Book.java
+++ src/main/java/com/mycompany/app/entities/Book.java
@@ -2,10 +2,15 @@ package com.mycompany.app.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Book {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "book_id")
   private int id;
 
