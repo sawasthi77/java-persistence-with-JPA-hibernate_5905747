e3cfed99e7ba821c870206c11fd47e86b1c30105
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-16 07:21:37 +0000

    End state for 04_04

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import com.mycompany.app.entities.Book;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -9,4 +11,18 @@ public class Main {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_persistence_unit");
 
     EntityManager em = emf.createEntityManager(); // Represent the persistence context
+
+    try {
+      em.getTransaction().begin();
+
+      Book b = new Book();
+      b.setName("my book");
+      b.setIsbn("123-456");
+      em.persist(b);
+
+      em.getTransaction().commit();
+    } finally {
+      em.close();
+    }
+  }
 }
