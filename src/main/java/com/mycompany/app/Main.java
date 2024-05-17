80f3e1f8ba456f398c24b1207ff55342ba459522
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-17 11:22:49 +0530

    Update Main.java

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -10,7 +10,7 @@ public class Main {
   public static void main(String[] args) {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_persistence_unit");
 
-    createInstance(emf);
+    //createInstance(emf);
 
   }
 
