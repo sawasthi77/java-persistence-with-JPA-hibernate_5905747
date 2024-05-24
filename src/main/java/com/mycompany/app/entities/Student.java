98021ac0762708267f2a0565cbd0b155c9169a06
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-24 06:39:00 +0000

    06_04 start state

---
+++ src/main/java/com/mycompany/app/entities/Student.java
@@ -0,0 +1,15 @@
+package com.mycompany.app.entities;
+
+public class Student extends Member {
+
+  private String studentCode;
+
+  public String getStudentCode() {
+    return studentCode;
+  }
+
+  public void setStudentCode(String studentCode) {
+    this.studentCode = studentCode;
+  }
+
+}
---
+++ src/main/java/com/mycompany/app/entities/Teacher.java
@@ -0,0 +1,14 @@
+package com.mycompany.app.entities;
+
+public class Teacher extends Member {
+
+  private String teacherCode;
+
+  public String getTeacherCode() {
+    return teacherCode;
+  }
+
+  public void setTeacherCode(String teacherCode) {
+    this.teacherCode = teacherCode;
+  }
+}
