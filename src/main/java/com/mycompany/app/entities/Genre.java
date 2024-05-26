65f82e6805ccf2500d6520125ebb26322425c929
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-26 15:27:15 +0000

    minor changes

--- src/main/java/com/mycompany/app/entities/Genre.java
+++ src/main/java/com/mycompany/app/entities/Genre.java
@@ -4,9 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
 import jakarta.persistence.Table;
-import jakarta.persistence.InheritanceType;
 
 @Entity
 @Table(name = "genre")
