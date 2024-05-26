b12ae595003185006edecd9f74ab8b3a2bb62e77
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-26 06:48:35 +0000

    06_06 start state

---
+++ mariadb-06_06.sql
@@ -0,0 +1,15 @@
+USE library;
+CREATE TABLE IF NOT EXISTS field(
+  field_id INT AUTO_INCREMENT PRIMARY KEY,
+  field_name VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS category(
+  category_id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS field_category(
+  field_id INT,
+  category_id INT,
+  FOREIGN KEY (field_id) REFERENCES field(field_id),
+  FOREIGN KEY (category_id) REFERENCES category(category_id)
+);
