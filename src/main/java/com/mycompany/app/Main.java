6e8c9ee5fb968e644afd3e3e3012d33e969d0417
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-31 11:09:28 +0000

    05_06 changes

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -31,31 +31,10 @@ public class Main {
   }
 
   private static void update(EntityManagerFactory emf) {
-    EntityManager em = emf.createEntityManager();
-    try {
-      em.getTransaction().begin();
 
-      Student s = em.find(Student.class, 1);
-      s.setName("Peter");
-
-      em.getTransaction().commit();
-    } finally {
-      em.close();
-    }
   }
 
   private static void attach(EntityManagerFactory emf) {
-    EntityManager em = emf.createEntityManager();
-    try {
-      em.getTransaction().begin();
 
-      Student s2 = new Student();
-      s2.setName("Mary");
-      em.merge(s2);
-
-      em.getTransaction().commit();
-    } finally {
-      em.close();
-    }
   }
 }
