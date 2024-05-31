189e74cfed8464b002610eae50adaaa99a9d1b69
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-31 06:40:01 +0000

    03_05 changes

--- mariadb-init.sql
+++ mariadb-init.sql
@@ -1,15 +1,33 @@
 -- Create a database
-DROP DATABASE IF EXISTS library;
-CREATE DATABASE IF NOT EXISTS library;
+DROP DATABASE IF EXISTS artclass;
+CREATE DATABASE IF NOT EXISTS artclass;
 -- Use the database
-USE library;
+USE artclass;
 -- Crate tables in the database
-CREATE TABLE IF NOT EXISTS book(
-  book_id INT AUTO_INCREMENT PRIMARY KEY,
-  book_name VARCHAR(255),
-  isbn VARCHAR(255)
+CREATE TABLE IF NOT EXISTS class(
+  class_id INT AUTO_INCREMENT PRIMARY KEY,
+  class_name VARCHAR(255),
+  day_of_week VARCHAR(255)
 );
-CREATE TABLE IF NOT EXISTS author(
-  author_id INT AUTO_INCREMENT PRIMARY KEY,
-  author_name VARCHAR(255)
+CREATE TABLE IF NOT EXISTS teacher(
+  teacher_id INT AUTO_INCREMENT PRIMARY KEY,
+  teacher_name VARCHAR(255)
 );
+CREATE TABLE IF NOT EXISTS student(
+  student_id INT AUTO_INCREMENT PRIMARY KEY,
+  student_name VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS student_class(
+  student_id INT,
+  class_id INT,
+  FOREIGN KEY (student_id) REFERENCES student(student_id),
+  FOREIGN KEY (class_id) REFERENCES class(class_id)
+);
+ALTER TABLE IF EXISTS class
+ADD COLUMN teacher_id INT;
+ALTER TABLE class
+ADD CONSTRAINT fk_teacher_id FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id);
+ALTER TABLE IF EXISTS teacher
+ADD COLUMN class_id INT;
+ALTER TABLE teacher
+ADD CONSTRAINT fk_class_id FOREIGN KEY (class_id) REFERENCES class(class_id);
