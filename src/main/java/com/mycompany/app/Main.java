8149f363a514038819e6378ea013f6452570a7d9
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-05-31 07:45:23 +0000

    change the main class

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -6,8 +6,6 @@ import jakarta.persistence.Persistence;
 
 public class Main {
   public static void main(String[] args) {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_persistence_unit");
 
-    EntityManager em = emf.createEntityManager(); // Represent the persistence context
   }
 }
