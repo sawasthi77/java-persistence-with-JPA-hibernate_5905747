68bc1db640f302ce6300459545e4f19e76e50e6f
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-06-01 11:54:09 +0000

    more changes

---
+++ src/main/java/com/mycompany/app/entities/Review.java
@@ -0,0 +1,46 @@
+package com.mycompany.app.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "review_id")
+  private int id;
+
+  private String comment;
+
+  private int rating;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public void setRating(int rating) {
+    this.rating = rating;
+  }
+}
