b500b272b7e12e8242068f3d6ffb351e3dbbfed5
Buddhini Samarakkody <buddhinisam@hotmail.com>
2024-07-12 08:33:41 +0530

    Update Main.java

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -83,95 +83,14 @@ public class Main {
   }
 
   private static void oneToOneRelationship(EntityManagerFactory emf) {
-    EntityManager em = emf.createEntityManager();
 
-    try {
-      em.getTransaction().begin();
-
-      ArtClass c = new ArtClass();
-      c.setName("Oil Painting");
-      c.setDayOfWeek("Monday");
-
-      Teacher t = new Teacher();
-      t.setName("Brown");
-
-      c.setTeacher(t);
-
-      em.persist(c);
-      em.persist(t);
-
-      em.getTransaction().commit();
-    } finally {
-      em.close();
-    }
   }
 
   private static void oneToManyRelationship(EntityManagerFactory emf) {
-    EntityManager em = emf.createEntityManager();
 
-    try {
-      em.getTransaction().begin();
-
-      Teacher t = new Teacher();
-      t.setName("Woods");
-
-      Review r1 = new Review();
-      r1.setComment("Excellent!");
-      r1.setRating(5);
-      r1.setTeacher(t);
-
-      Review r2 = new Review();
-      r2.setComment("Good!");
-      r2.setRating(4);
-      r2.setTeacher(t);
-
-      t.setReviews(List.of(r1, r2));
-
-      em.persist(t);
-
-      em.getTransaction().commit();
-    } finally {
-      em.close();
-    }
   }
 
   private static void manyToManyRelationship(EntityManagerFactory emf) {
-    EntityManager em = emf.createEntityManager();
-
-    try {
-      em.getTransaction().begin();
-
-      ArtClass c1 = new ArtClass();
-      c1.setName("Recycled Scrap Art");
-      c1.setDayOfWeek("Tuesday");
-
-      ArtClass c2 = new ArtClass();
-      c2.setName("Drawing Hands");
-      c2.setDayOfWeek("Wednesday");
-
-      Teacher t1 = new Teacher();
-      t1.setName("White");
-
-      Teacher t2 = new Teacher();
-      t2.setName("Wood");
-
-      c1.setTeacher(t1);
-      c2.setTeacher(t2);
-
-      Student s1 = new Student();
-      s1.setName("Anne");
-      Student s2 = new Student();
-      s2.setName("Martin");
-
-      c1.setStudents(List.of(s1, s2));
-      c2.setStudents(List.of(s1, s2));
-
-      em.persist(c1);
-      em.persist(c2);
-
-      em.getTransaction().commit();
-    } finally {
-      em.close();
-    }
+   
   }
 }
